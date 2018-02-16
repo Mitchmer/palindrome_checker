4fa225ba3aead6c6bd33d4a91889b618ef982c10
Mitch Merrell <mitchmerrell88@gmail.com>
2018-02-16 10:49:13 -0700

    got the check to work with spaces

--- brainteaser2.rb
+++ brainteaser2.rb
@@ -1,24 +1,28 @@
-# - ask user for input of palindrome
-#   - option to quit
-# - check if palindrome
-#   - remove spaces
-#   - join
-#   - split
-require 'pry'
-
 def user_input
   puts "What would you like to input as your palindrome?"
-  @input = gets.strip
-  checkinput
+  input = gets.strip
+  checkinput(input)
 end
 
-def checkinput
-  checkprep = @input.gsub(/[ ]/, '').split(//)
+def checkinput(word)
+  checkprep = word.gsub(/[ ]/, '').split(//)
+  initialarr = checkprep.map {|x| x}
   reversearr = []
   while checkprep.length > 0
     reversearr << checkprep.pop
   end
-  binding.pry
+  if initialarr == reversearr
+    puts "What up! Nice pallindrome"
+  else
+    puts "That's definitely not a pallindrome. Try again."
+  end
+  puts "Would you like to try again? (y/n)"
+  choice = gets.strip
+  if choice == 'y'
+    user_input
+  else
+    exit
+  end
 end
 
 user_input
